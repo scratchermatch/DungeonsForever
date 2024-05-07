4c5ed558571dd062cfbd7396b08b00c0c4fd02d3
Alexander Hearn <alexanderhappyowl@gmail.com>
2024-05-07 14:53:38 -0600

    Made the character load screen functional

--- Scripts/UI/LoadCharacterScreen/CharacterFileDisplay.gd
+++ Scripts/UI/LoadCharacterScreen/CharacterFileDisplay.gd
@@ -3,15 +3,15 @@ extends Control
 @onready var file_v_box = %FileVBox
 
 const PLAYER_FILE_BOX = preload("res://Scenes/player_file_box.tscn")
-var filepath = "user://"
+var filepath_prefix = "user://"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var dir = DirAccess.open(filepath)
+	var dir = DirAccess.open(filepath_prefix)
 	if dir:
 		var files = dir.get_files()
 		for file in files:
 			if file.ends_with(".dfc"):
 				var file_button = PLAYER_FILE_BOX.instantiate()
-				var player_data = DataLoader.load_file(filepath + file)
+				var player_data = DataLoader.load_file(filepath_prefix + file)
 				file_v_box.add_child(file_button)
 				file_button.init(player_data)
--- Scripts/UI/MainMenu/CreateCharacterButton.gd
+++ Scripts/UI/MainMenu/CreateCharacterButton.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/character_creation_screen.tscn")
+	get_tree().change_scene_to_file("res://Scenes/create_character_screen.tscn")
